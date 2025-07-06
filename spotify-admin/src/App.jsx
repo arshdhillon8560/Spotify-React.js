7143790afef2376b927e15a0d7bd21ec6474b88e
Arshdeep Singh <139613812+arshdhillon8560@users.noreply.github.com>
2025-07-06 16:53:04 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -8,7 +8,7 @@ import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url='http://localhost:4000'
+export const url='https://spotify-react-js-delta.vercel.app'
 
 const App = () => {
   return (
